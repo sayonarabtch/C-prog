90f54450953b00d049c503e317615f880f725163
NIKITA <sayonarabtch@gmail.com>
2018-03-15 23:34:57 +0300

    complete

---
+++ 3.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+
+int main(void) {
+	int x, y;
+	x = 15; y = 87;
+
+    int z = y - x ;
+
+    printf("%i вычесть из %i равно %i", x, y, z);
+}
---
+++ 4.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	int sum;
+	//Подсчитать результат
+	sum = 25 + 37 - 19 ;
+	/*Отобразить результат */
+	printf("The answer is %i \n", sum);
+	return 0;
+}
